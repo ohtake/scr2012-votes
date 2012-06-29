d0fabee16cfe314394377c39b34951bbd189d32c
OHTAKE Tomohiro <tomohiro@kuramae.ne.jp>
2012-06-29 23:07:47 +0900

    Initial script

---
+++ scr2012-votes.ps1
@@ -0,0 +1,59 @@
+﻿$baseUri = "http://sanriocharacterranking.com/"
+$lowerUri = $baseUri + "rankingall.php"
+
+$date = (Get-Date).Date
+
+function wget([String]$uri) {
+    $wc = New-Object Net.WebClient
+    $wc.Encoding = [Text.Encoding]::UTF8
+    $wc.DownloadString($uri)
+}
+function new-resultline([int]$rank, [int]$id, [string]$name, [int]$votes) {
+    New-Object PSObject -Property @{
+        Date = $date;
+        Rank = $rank;
+        CID = $id;
+        Name = $name;
+        Votes = $votes;
+    }
+}
+function get-top10() {
+    $body = wget($baseUri)
+    $body -split "<li " |
+        % {
+            if ($_ -match "rank(\d+)") {
+                $rank = [int]$Matches[1]
+            } else { return }
+            if ($_ -match " chr(\d+)") {
+                $id = [int]$Matches[1]
+            } else { return }
+            if ($_ -match "<strong><span>(.+)</span></strong>") {
+                $name = $Matches[1]
+            } else { return }
+            if ($_ -match "得票数&nbsp;([\d,]+)票") {
+                $votes = [int]$Matches[1]
+            } else { return }
+            new-resultline $rank $id $name $votes
+        }
+}
+function get-lower() {
+    $body = wget($lowerUri)
+    $body -split "<li " |
+        % {
+            if ($_ -match "(\d+)位") {
+                $rank = [int]$Matches[1]
+            } else { return }
+            if ($_ -match "chr(\d+)") {
+                $id = [int]$Matches[1]
+            } else { return }
+            if ($_ -match "<strong>(.+)</strong>") {
+                $name = $Matches[1]
+            } else { return }
+            if ($_ -match "([\d,]+)&nbsp;票") {
+                $votes = [int]$Matches[1]
+            } else { return }
+            new-resultline $rank $id $name $votes
+        }
+}
+
+$votes = @(get-top10) + @(get-lower)
